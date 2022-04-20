36c627a9f52ed6307f2962996a1808d65d8af1f6
Shruti Kapoor <shrkapoor@paypal.com>
2022-04-19 22:25:19 -0700

    adding text

--- components/Newsletter.js
+++ components/Newsletter.js
@@ -1,6 +1,6 @@
 export default function Newsletter() {
   return (
-    <section className="text-gray-600 body-font">
+    <section className="body-font">
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-col text-center w-full mb-12">
           <h2 className="sm:text-3xl text-2xl font-medium title-font mb-4">
@@ -28,13 +28,13 @@ export default function Newsletter() {
                 id="tlemail"
                 name="email"
                 placeholder="Email"
-                className="bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 mx-3 leading-8 transition-colors duration-200 ease-in-out"
+                className="bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none py-1 px-3 mx-3 leading-8 transition-colors duration-200 ease-in-out"
               />
               <input type="hidden" value="1" name="embed" />
               <input
                 type="submit"
                 value="Subscribe"
-                className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                className="bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
               />
             </form>
           </div>
