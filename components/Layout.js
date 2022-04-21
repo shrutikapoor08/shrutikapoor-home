454e97325518b79eb7f912872654ae95373af64e
Shruti Kapoor <shrkapoor@paypal.com>
2022-04-21 11:55:57 -0700

    fixing width

--- components/Layout.js
+++ components/Layout.js
@@ -51,7 +51,7 @@ export default function Layout({ children }) {
 
   return (
     <div className="relative pb-24 overflow-hidden">
-      <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
+      <div className="flex flex-col items-center max-w-3xl w-full mx-auto">
         {children}
       </div>
     </div>
--- components/Newsletter.js
+++ components/Newsletter.js
@@ -6,7 +6,7 @@ export default function Newsletter() {
           <h2 className="sm:text-3xl text-2xl font-medium title-font mb-4">
             Get articles like this in your inbox
           </h2>
-          <p> I will not spam. Ever.</p>
+          <p> I will not spam. I will not sell your data. Ever.</p>
         </div>
         <div className="flex w-full sm:flex-row flex-row mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
           <div className="relative flex-grow w-full">
@@ -28,7 +28,7 @@ export default function Newsletter() {
                 id="tlemail"
                 name="email"
                 placeholder="Email"
-                className="bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none py-1 px-3 mx-3 leading-8 transition-colors duration-200 ease-in-out"
+                className="bg-gray-200 bg-opacity-50 rounded border border-gray-100 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none py-1 px-3 mx-3 leading-8 transition-colors duration-200 ease-in-out"
               />
               <input type="hidden" value="1" name="embed" />
               <input
