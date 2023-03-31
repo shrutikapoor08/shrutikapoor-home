ba7a939059a9373c74e9707acb486e8788f622c7
shrutikapoor08 <shruti.kapoor08@gmail.com>
2023-03-31 11:06:06 -0700

    Adding canvas

--- components/Newsletter.js
+++ components/Newsletter.js
@@ -1,7 +1,7 @@
 export default function Newsletter() {
   return (
     <section className="body-font">
-      <div className="container px-5 py-8 mx-auto mt-12 ">
+      <div className="container px-5 py-8 mx-auto mt-12 mb-24">
         <div className="flex flex-col text-center w-full mb-12">
           <h2 className="sm:text-3xl text-2xl font-medium title-font mb-4">
             Get latest blog articles in your inbox.
@@ -38,6 +38,12 @@ export default function Newsletter() {
               />
             </div>
           </form>
+
+          <div className="flex flex-col sm:flex-row gap-4 items-center justify-center">
+            <a href="https://shrutikapoor.substack.com/?utm_source=substack&utm_medium=web&utm_campaign=substack_profile">
+              View previous newsletters
+            </a>
+          </div>
         </div>
       </div>
     </section>
