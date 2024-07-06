59b50add8ce21786c30203165da78547b2b700bd
shrutikapoor08 <shruti.kapoor08@gmail.com>
2024-07-05 17:13:47 -0700

    Fixing header

--- components/Header.js
+++ components/Header.js
@@ -5,9 +5,9 @@ export default function Header({ name }) {
   const globalData = getGlobalData();
 
   return (
-    <header className="top-0">
+    <header className="mt-5">
       <nav className="flex items-center justify-between flex-wrap bg-teal p-6 menu">
-        <div className="block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div className="block text-xl flex-grow lg:flex lg:items-center lg:w-auto">
           <Link href="/">Home</Link>
           <Link href="/blog">Blog</Link>
           <Link href="/talks">Talks</Link>
